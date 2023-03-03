f97ad559414a2a4e916812f7ae72292af7ab04be
Nelson Melo <nmelo.cu@gmail.com>
2023-03-03 15:51:55 -0500

    Basic web server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func serve(w http.ResponseWriter, req *http.Request) {
+	_, err := fmt.Fprintf(w, "hello\n")
+	if err != nil {
+		return
+	}
+}
 
 func main() {
-	fmt.Println("test")
+	http.HandleFunc("/", serve)
+	fmt.Println("Serving on port: 8090. Press Ctrl-C to stop.")
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil {
+		fmt.Printf("%s", err)
+		return
+	}
 }
